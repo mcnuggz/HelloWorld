44f65b58ddc9c4419fcd9f8fe092332776112b4a
Ryan Webb <dzanfox@gmail.com>
2017-09-06 02:07:34 -0500

    data saving and reading added

---
+++ HelloWorld/DataStorage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public static class DataStorage
+    {
+        
+        public static void SaveMessage(string message, string name)
+        {
+            OutputFunctions outputFunctions = new OutputFunctions();
+            Console.WriteLine(Environment.NewLine);
+            string confirmationMessage = "Message Saved!";
+            using (StreamWriter writer = new StreamWriter(Environment.GetFolderPath(System.Environment.SpecialFolder.DesktopDirectory) + @"\Messages.txt", true))
+            {
+                writer.WriteLine(DateTime.Now + ": " + message + " (" + name + ")");                
+            }
+            Console.WriteLine(confirmationMessage + Environment.NewLine);
+            outputFunctions.LeaveAMessage();
+
+
+        }
+
+        public static void DisplayMessages()
+        {
+            string line = "";
+            using (StreamReader reader = new StreamReader(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + @"\Messages.txt"))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+                Console.WriteLine(Environment.NewLine);
+            }
+        }
+    }
+}
--- HelloWorld/OutputFunctions.cs
+++ HelloWorld/OutputFunctions.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace HelloWorld
 {
@@ -12,42 +9,62 @@ namespace HelloWorld
         public void SayHelloWorld()
         {
             Console.WriteLine("Hello World");
-            Console.ReadKey();
+            Thread.Sleep(2000);
             LeaveAMessage();
         }
 
-        private bool LeaveAMessage()
+        public bool LeaveAMessage()
         {
             bool wouldLikeToContinue = false;
             string userInput = "";
+
             while (wouldLikeToContinue == false)
             {
-                Console.WriteLine("Would you like to add a message for someone else? Y/N");
+                Console.WriteLine("Would you like to add a message? Y/N");
+                //takes input and lowercases it for consistency
                 userInput = Console.ReadLine().ToLower();
-                if (userInput == "y" || userInput == "yes")
+                if (userInput == "y" || userInput == "yes" || userInput == "yep")
                 {
                     wouldLikeToContinue = true;
                     EnterMessage();
                 }
-                else if (userInput == "n" || userInput == "no")
+                else if (userInput == "n" || userInput == "no" || userInput == "nope")
                 {
-                    Console.WriteLine("Very well, this program will close in five seconds. Thank you and have a good day!");
-                    Thread.Sleep(5000);
-                    Environment.Exit(0);
+                    Console.WriteLine("Read all Messages or Close?");
+                    string userInput2 = Console.ReadLine().ToLower();
+                    if (userInput2 == "read all messages" || userInput2 == "read")
+                    {
+                        DataStorage.DisplayMessages();
+                    }
+                    else
+                    {
+                        CloseApp();
+                    }                   
                 }
                 else
                 {
-                    Console.WriteLine("Please try again!");
+                    Console.WriteLine("Please try again!" + Environment.NewLine);
                 }
             }
-
             return wouldLikeToContinue;
         }
 
         private void EnterMessage()
         {
-            Console.WriteLine("This works.");
-            Console.ReadKey();
+            Console.WriteLine("Please leave your message now!");
+            string userMessage = "";
+            userMessage = Console.ReadLine();
+            string userName = "";
+            Console.WriteLine("Enter your name!");
+            userName = Console.ReadLine();
+            DataStorage.SaveMessage(userMessage, userName);
+        }
+
+        public void CloseApp()
+        {
+            Console.WriteLine("This program will close in five seconds. You can find the text file with your messages on your desktop. Thank you and have a good day!");
+            Thread.Sleep(5000);
+            Environment.Exit(0);
         }
     }
 }
