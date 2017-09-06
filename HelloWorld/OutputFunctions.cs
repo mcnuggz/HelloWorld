4487884bcbcfac3dfc2765cc235adede3af3f5a3
Ryan Webb <dzanfox@gmail.com>
2017-09-06 01:04:09 -0500

    added user input for program continuation

--- HelloWorld/OutputFunctions.cs
+++ HelloWorld/OutputFunctions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace HelloWorld
@@ -11,6 +12,41 @@ namespace HelloWorld
         public void SayHelloWorld()
         {
             Console.WriteLine("Hello World");
+            Console.ReadKey();
+            LeaveAMessage();
+        }
+
+        private bool LeaveAMessage()
+        {
+            bool wouldLikeToContinue = false;
+            string userInput = "";
+            while (wouldLikeToContinue == false)
+            {
+                Console.WriteLine("Would you like to add a message for someone else? Y/N");
+                userInput = Console.ReadLine().ToLower();
+                if (userInput == "y" || userInput == "yes")
+                {
+                    wouldLikeToContinue = true;
+                    EnterMessage();
+                }
+                else if (userInput == "n" || userInput == "no")
+                {
+                    Console.WriteLine("Very well, this program will close in five seconds. Thank you and have a good day!");
+                    Thread.Sleep(5000);
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("Please try again!");
+                }
+            }
+
+            return wouldLikeToContinue;
+        }
+
+        private void EnterMessage()
+        {
+            Console.WriteLine("This works.");
             Console.ReadKey();
         }
     }
